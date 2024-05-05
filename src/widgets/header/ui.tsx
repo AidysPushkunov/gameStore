d70732366c885279c4d2a8ccd34bbc0d07d692fb
Aidys Pushkunov <mywayinprogramming@gmail.com>
2024-05-06 00:01:09 +0700

    shown search input only product page

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -5,18 +5,26 @@ import basketIco from 'shared/assets/images/basket.svg';
 import avatarIco from 'shared/assets/images/profile.svg';
 import { Burger } from 'entities/burger';
 import { SearchGames } from 'features/searchGame';
+import { useLocation } from 'react-router-dom'
 
 
 const Header: React.FC = () => {
+    const location = useLocation();
+    
+
+    console.log(location.pathname);
+
     return (
         <header className={styles.header}>
             <div className={styles.burger}>
                 <Burger />
             </div>
-            <div className={styles.search_element}>
-                <SearchGames />
-            </div>
-
+            {
+                location.pathname === '/' ? ( <div className={styles.search_element}>
+                    <SearchGames />
+                </div>) : (<div></div>)
+            }
+      
             <div className={styles.elements}>
                 <img className={styles.image} src={notificationIco} alt='notification' />
                 <img className={styles.image} src={basketIco} alt='basket' />
